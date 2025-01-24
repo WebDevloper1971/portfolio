fac6a42ff411567847f11b20c2cc5ce34ea5a99d
Vedant <webtech1971@gmail.com>
2025-01-24 14:56:30 +0530

    added light-dark mode

---
+++ script.js
@@ -0,0 +1,288 @@
+const projectList = [
+  {
+    name: "Velet",
+    description:
+      "Timepieces Ecommerce Platform made with Next JS Framework, Prisma ORM, OAuth features for Authentication and Authorization with Modern yet Classic User Interface made with Figma and implemented with Tailwind CSS and Daisy UI. MongoDB Atlas (NoSQL Database) is used for storing data.",
+    image: "./project/ecomm.png",
+    link: "https://ecommerce-next-js-mauve-rho.vercel.app/",
+    svg: "./link.svg",
+  },
+  {
+    name: "Eco-Blog",
+    description:
+      "Timepieces Ecommerce Platform made with Next JS Framework, Prisma ORM, OAuth features for Authentication and Authorization with Modern yet Classic User Interface made with Figma and implemented with Tailwind CSS and Daisy UI. MongoDB Atlas (NoSQL Database) is used for storing data.",
+    image: "./project/ecoblog.png",
+    link: "https://ecommerce-next-js-mauve-rho.vercel.app/",
+    svg: "/link.svg",
+  },
+  {
+    name: "VouTube",
+    description:
+      "Timepieces Ecommerce Platform made with Next JS Framework, Prisma ORM, OAuth features for Authentication and Authorization with Modern yet Classic User Interface made with Figma and implemented with Tailwind CSS and Daisy UI. MongoDB Atlas (NoSQL Database) is used for storing data.",
+    image: "./project/yt.png",
+    link: "https://ecommerce-next-js-mauve-rho.vercel.app/",
+    svg: "/link.svg",
+  },
+  {
+    name: "AMP Project",
+    description:
+      "Timepieces Ecommerce Platform made with Next JS Framework, Prisma ORM, OAuth features for Authentication and Authorization with Modern yet Classic User Interface made with Figma and implemented with Tailwind CSS and Daisy UI. MongoDB Atlas (NoSQL Database) is used for storing data.",
+    image: "./project/amp.png",
+    link: "https://ecommerce-next-js-mauve-rho.vercel.app/",
+    svg: "/link.svg",
+  },
+  {
+    name: "OTP Auth",
+    description:
+      "Timepieces Ecommerce Platform made with Next JS Framework, Prisma ORM, OAuth features for Authentication and Authorization with Modern yet Classic User Interface made with Figma and implemented with Tailwind CSS and Daisy UI. MongoDB Atlas (NoSQL Database) is used for storing data.",
+    image: "./project/otpAuth.png",
+    link: "https://ecommerce-next-js-mauve-rho.vercel.app/",
+    svg: "/link.svg",
+  },
+  {
+    name: "Password Manager",
+    description:
+      "Timepieces Ecommerce Platform made with Next JS Framework, Prisma ORM, OAuth features for Authentication and Authorization with Modern yet Classic User Interface made with Figma and implemented with Tailwind CSS and Daisy UI. MongoDB Atlas (NoSQL Database) is used for storing data.",
+    image: "./project/passwordManager.png",
+    link: "https://ecommerce-next-js-mauve-rho.vercel.app/",
+    svg: "/link.svg",
+  },
+  {
+    name: "Algorithm Visualizer",
+    description:
+      "Timepieces Ecommerce Platform made with Next JS Framework, Prisma ORM, OAuth features for Authentication and Authorization with Modern yet Classic User Interface made with Figma and implemented with Tailwind CSS and Daisy UI. MongoDB Atlas (NoSQL Database) is used for storing data.",
+    image: "./project/algo-visualiser.png",
+    link: "https://ecommerce-next-js-mauve-rho.vercel.app/",
+    svg: "/link.svg",
+  },
+  {
+    name: "Velet",
+    description:
+      "Timepieces Ecommerce Platform made with Next JS Framework, Prisma ORM, OAuth features for Authentication and Authorization with Modern yet Classic User Interface made with Figma and implemented with Tailwind CSS and Daisy UI. MongoDB Atlas (NoSQL Database) is used for storing data.",
+    image: "",
+    link: "https://ecommerce-next-js-mauve-rho.vercel.app/",
+    svg: "/link.svg",
+  },
+];
+
+const techList = [
+  {
+    id: 1,
+    image:
+      "https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original.svg",
+    link: "https://www.java.com",
+  },
+  {
+    id: 2,
+    image: "https://www.vectorlogo.zone/logos/springio/springio-icon.svg",
+    link: "https://spring.io/",
+  },
+  {
+    id: 3,
+    image:
+      "https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original-wordmark.svg",
+    link: "https://react.dev/",
+  },
+  {
+    id: 4,
+    image: "https://cdn.worldvectorlogo.com/logos/nextjs-2.svg",
+    link: "https://nextjs.org/",
+  },
+  {
+    id: 5,
+    image:
+      "https://raw.githubusercontent.com/devicons/devicon/master/icons/android/android-original-wordmark.svg",
+    link: "https://developer.android.com",
+  },
+  {
+    id: 6,
+    image:
+      "https://raw.githubusercontent.com/devicons/devicon/master/icons/bootstrap/bootstrap-plain-wordmark.svg",
+    link: "https://getbootstrap.com",
+  },
+  {
+    id: 7,
+    image:
+      "https://raw.githubusercontent.com/devicons/devicon/master/icons/docker/docker-original-wordmark.svg",
+    link: "https://www.docker.com/",
+  },
+  {
+    id: 8,
+    image:
+      "https://raw.githubusercontent.com/devicons/devicon/master/icons/express/express-original-wordmark.svg",
+    link: "https://expressjs.com",
+  },
+  {
+    id: 9,
+    image: "https://www.vectorlogo.zone/logos/figma/figma-icon.svg",
+    link: "https://www.figma.com/",
+  },
+  {
+    id: 10,
+    image: "https://www.vectorlogo.zone/logos/git-scm/git-scm-icon.svg",
+    link: "https://git-scm.com/",
+  },
+  {
+    id: 11,
+    image:
+      "https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original-wordmark.svg",
+    link: "https://www.w3.org/html/",
+  },
+  {
+    id: 12,
+    image:
+      "https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original-wordmark.svg",
+    link: "https://www.w3schools.com/css/",
+  },
+  {
+    id: 13,
+    image:
+      "https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg",
+    link: "https://developer.mozilla.org/en-US/docs/Web/JavaScript",
+  },
+
+  {
+    id: 14,
+    image:
+      "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg",
+    link: "https://www.mongodb.com/",
+  },
+
+  {
+    id: 15,
+    image:
+      "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+    link: "https://www.mysql.com/",
+  },
+
+  {
+    id: 16,
+    image:
+      "https://raw.githubusercontent.com/devicons/devicon/master/icons/nodejs/nodejs-original-wordmark.svg",
+    link: "https://nodejs.org",
+  },
+  {
+    id: 17,
+    image:
+      "https://raw.githubusercontent.com/devicons/devicon/master/icons/postgresql/postgresql-original-wordmark.svg",
+    link: "https://www.postgresql.org/",
+  },
+  {
+    id: 18,
+    image:
+      "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+    link: "https://www.python.org",
+  },
+  {
+    id: 19,
+    image: "https://www.vectorlogo.zone/logos/tailwindcss/tailwindcss-icon.svg",
+    link: "https://tailwindcss.com/",
+  },
+  {
+    id: 20,
+    image: "https://www.vectorlogo.zone/logos/getpostman/getpostman-icon.svg",
+    link: "https://postman.com",
+  },
+];
+
+const categoryList = ["All", "Java", "Web", "Design"];
+const navigationLinks = document.querySelectorAll(".navigationLinks button");
+const navigationLinksMobile = document.getElementById(
+  "navigation-links-mobile"
+);
+const themeToggler = document.getElementById("theme-toggler");
+const gridContainer = document.getElementById("grid-container");
+const techGridContainer = document.getElementById("tech-grid");
+
+function navigate(id) {
+  const element = document.getElementById(id);
+  if (element) {
+    element.scrollIntoView({ behavior: "smooth" });
+  }
+}
+
+function createCard(name, image, link, description, linkSVG) {
+  const card = document.createElement("li");
+  card.classList.add("project-card");
+
+  const projectImage = document.createElement("img");
+  projectImage.src = image;
+  projectImage.classList.add("project-image");
+
+  const overlay = document.createElement("div");
+  overlay.classList.add("overlay");
+
+  const linkSection = document.createElement("div");
+  linkSection.classList.add("project-link-section");
+
+  const anchor = document.createElement("a");
+  anchor.href = link;
+  anchor.target = "_blank";
+
+  const linkImage = document.createElement("img");
+  linkImage.src = linkSVG;
+
+  anchor.appendChild(linkImage);
+
+  const projectName = document.createElement("h2");
+  projectName.textContent = name;
+
+  linkSection.appendChild(projectName);
+  linkSection.appendChild(anchor);
+
+  const projectDescription = document.createElement("p");
+  projectDescription.textContent = description;
+
+  overlay.appendChild(linkSection);
+  overlay.appendChild(projectDescription);
+
+  card.appendChild(projectImage);
+  card.appendChild(overlay);
+
+  return card;
+}
+
+function techLogo(id, image, link) {
+  const card = document.createElement("li");
+  card.classList.add("tech-card");
+
+  const anchor = document.createElement("a");
+  anchor.href = link;
+
+  const projectImage = document.createElement("img");
+  projectImage.src = image;
+
+  card.appendChild(anchor);
+  anchor.appendChild(projectImage);
+
+  return card;
+}
+
+navigationLinksMobile.addEventListener("change", (e) => {
+  navigate(e.target.value);
+});
+
+projectList.map((project, index) => {
+  gridContainer.appendChild(
+    createCard(
+      project.name,
+      project.image,
+      project.link,
+      project.description,
+      project.svg
+    )
+  );
+});
+
+techList.map((tech, index) => {
+  techGridContainer.appendChild(techLogo(tech.id, tech.image, tech.link));
+});
+
+themeToggler.addEventListener("click", () => {
+  const theme = document.querySelector("html").getAttribute("data-theme");
+  if (theme === "light") {
+    document.querySelector("html").setAttribute("data-theme", "dark");
+  } else {
+    document.querySelector("html").setAttribute("data-theme", "light");
+  }
+});
